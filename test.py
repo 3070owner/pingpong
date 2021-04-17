85d2a839abccb79ba046e47e872f6ffca78137e4
unknown <maetan.s200406@ggh.goe.go.kr>
2021-04-17 10:49:11 +0900

    pingpong.2

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+
+
+
+
+#범위설정 잘못함
+
+
+
+
+
 import pygame
 import sys
 import random
 import time
 
-COLOR = {"BLACK":(0,0,0),"WHITE":(255,255,255),"RED":(255,0,0)}
+COLOR = {"BLACK":(0,0,0),"WHITE":(255,255,255),"RED":(255,0,0),"GREEN":(0,255,0)}
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 #How 2 use: COLOR["BLACK"]
@@ -22,7 +32,7 @@ class Ball:
         #time.sleep(3)
 
 class Bar:
-    def __init__(self, bar_speed = 0.6, left_pos_bar = [40, 400], right_pos_bar = [760, 400] ):
+    def __init__(self, bar_speed = 0.42, left_pos_bar = [40, 400], right_pos_bar = [760, 400] ):
         self.bar_speed = bar_speed
         self.left_pos_bar = left_pos_bar
         self.right_pos_bar = right_pos_bar
@@ -39,9 +49,11 @@ class Bar:
         if key_event[pygame.K_DOWN]:
             self.right_pos_bar[1] += self.bar_speed
 
-class Game():
-    def __init__(self, screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)), Lp_point = 0, Rp_point = 0):
 
+class Game:
+    def __init__(self, screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)), Lp_point = 0, Rp_point = 0, Rbarlen=40, Lbarlen=40):
+        self.Rbarlen = Rbarlen
+        self.Lbarlen = Lbarlen
         self.screen = screen
         self.Rp_point = Rp_point
         self.Lp_point = Lp_point
@@ -50,42 +62,58 @@ class Game():
         pygame.init()
         pygame.display.set_caption("pygame")
 
-
     def draw(self):
         self.screen.fill(COLOR["BLACK"])
-        pygame.draw.rect(self.screen, COLOR["WHITE"], pygame.Rect(self.a_2.left_pos_bar[0], self.a_2.left_pos_bar[1],5,50))
-        pygame.draw.rect(self.screen, COLOR["WHITE"], pygame.Rect(self.a_2.right_pos_bar[0], self.a_2.right_pos_bar[1],5,50))
+        pygame.draw.rect(self.screen, COLOR["WHITE"], pygame.Rect(self.a_2.left_pos_bar[0], self.a_2.left_pos_bar[1],5,self.Lbarlen))
+        pygame.draw.rect(self.screen, COLOR["WHITE"], pygame.Rect(self.a_2.right_pos_bar[0], self.a_2.right_pos_bar[1],5,self.Rbarlen))
         pygame.draw.circle(self.screen, COLOR["RED"],[self.a_1.pos_ball[0],self.a_1.pos_ball[1]] ,5)
         pygame.display.update()
 
+
     def score(self):
         if self.a_1.pos_ball[0]<5:
-            self.Rp_point +=1
-            print('right player +1')
+            self.Rp_point +=2
+            print('right player +2')
             self.a_1.ball_start()
             print("right player score:",self.Rp_point)
         elif self.a_1.pos_ball[0]>795:
-            self.Lp_point +=1
-            print('left player +1')
+            self.Lp_point +=2
+            print('left player +2')
             self.a_1.ball_start()
             print("left player score:",self.Lp_point)
     def ball_move(self):
 
-        if self.a_1.pos_ball[0] - self.a_2.left_pos_bar[0] <=2:
-            if abs(self.a_2.left_pos_bar[1] - self.a_1.pos_ball[1])<30:
+        if (self.a_1.pos_ball[1] - self.a_2.left_pos_bar[1]) <= self.Lbarlen and (self.a_1.pos_ball[1]-self.a_2.left_pos_bar[1]) >=0:
+            if abs(self.a_2.left_pos_bar[0] - self.a_1.pos_ball[0]) <=1:
                 self.a_1.x_speed_ball = abs(self.a_1.x_speed_ball)
+                print("done")
 
-        if self.a_2.right_pos_bar[0] - self.a_1.pos_ball[0] <=2:
-            if abs(self.a_2.right_pos_bar[1] - self.a_1.pos_ball[1])<30:
+        if (self.a_1.pos_ball[1] - self.a_2.right_pos_bar[1]) <= self.Rbarlen and (self.a_1.pos_ball[1] - self.a_2.right_pos_bar[1]) >=0:
+            if abs(self.a_2.right_pos_bar[0] - self.a_1.pos_ball[0]) <=1:
                 self.a_1.x_speed_ball = -abs(self.a_1.x_speed_ball)
+                print("done2")
 
-        if self.a_1.pos_ball[1]<0:
+        if self.a_1.pos_ball[1]<5:
             self.a_1.y_speed_ball = -self.a_1.y_speed_ball
-        elif self.a_1.pos_ball[1]>800:
+        elif self.a_1.pos_ball[1]>795:
             self.a_1.y_speed_ball = -self.a_1.y_speed_ball
         self.a_1.pos_ball[0] += self.a_1.x_speed_ball
         self.a_1.pos_ball[1] += self.a_1.y_speed_ball
+    def ItemCall(self):
+        key_event = pygame.key.get_pressed()
 
+        if key_event[pygame.K_0]:
+            if self.Rp_point>=1:
+                self.Rp_point-=1
+                self.Rbarlen+=random.randint(3, 7)
+                print(self.Rbarlen)
+
+
+        if key_event[pygame.K_1]:
+            if self.Lp_point>=1:
+                self.Lp_point-=1
+                self.Lbarlen += random.randint(3, 7)
+                print(self.Lbarlen)
 
 
 test = Game()
@@ -99,6 +127,14 @@ while True:
     test.a_2.bar_move()
     test.ball_move()
     test.draw()
+    test.ItemCall()
     #print(test.a_1.pos_ball)
     test.score()
-
+    if test.Rbarlen>100:
+        print('player2 win')
+        pygame.display.quit()
+        sys.exit()
+    elif test.Lbarlen>100:
+        print('player1 win')
+        pygame.display.quit()
+        sys.exit()
